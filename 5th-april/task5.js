dffd5967c6ebf482fdbfc812dd5781ebc2650e12
ajaygrt <82510646+ajaygrt@users.noreply.github.com>
2022-04-08 13:19:39 +0530

    Update task5.js

--- 5th-april/task5.js
+++ 5th-april/task5.js
@@ -1,7 +1,7 @@
-// let cars = ["Rolls","mcclaren","maruthi","honda"];
-// performgroup(cars);
-// function performgroup(cars)
-// {
-//     var grouped = cars.groupBy(cars,"length");
-//     console.log(grouped);
-// }
+let cars = ["Rolls","mcclaren","maruthi","honda"];
+performgroup(cars);
+function performgroup(cars)
+{
+    var grouped = cars.groupBy(cars,"length");
+    console.log(grouped);
+}
